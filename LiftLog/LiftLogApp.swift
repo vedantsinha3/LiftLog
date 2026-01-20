d22b38b20693ca47d4743ce696b5fbd054512671
Vedant Sinha <vedantsinha3@gmail.com>
2026-01-19 21:09:48 -0800

    Initial Commit

---
+++ LiftLog/LiftLogApp.swift
@@ -0,0 +1,32 @@
+//
+//  LiftLogApp.swift
+//  LiftLog
+//
+//  Created by Vedant Sinha on 1/19/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LiftLogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
